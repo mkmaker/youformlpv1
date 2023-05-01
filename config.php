241cc2df1871023a97b2d31884f70036db89b9ec
sudipa0304 <sudipamukherjee34@gmail.com>
2023-05-01 22:02:51 +0530

    Fix title and description

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
+    'title' => 'Youform - A Free Typeform alternative',
+    'description' => 'Youform gives you a drag-and-drop builder to make forms that your users will love to fill.',
     'collections' => [],
 ];
