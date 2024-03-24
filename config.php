61e7ccb8d43287f7cb838838883831511977d880
Abhishek <themkmaker@gmail.com>
2024-03-24 18:06:03 +0530

    Fix

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Str;
 
 return [
     'production' => false,
-    'baseUrl' => 'http://youform-may23.test/',
+    'baseUrl' => 'http://youform-may23.test',
     'title' => 'Youform - A Free Typeform alternative',
     'description' => 'Youform gives you a drag-and-drop builder to make forms that your users will love to fill.',
     'current' => function ($page, $section) {
