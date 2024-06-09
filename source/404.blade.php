6c01a1fe7f7ce75aa0807cd85607cb8eaf646224
Abhishek <themkmaker@gmail.com>
2024-06-10 01:04:32 +0530

    Improve 404

--- source/404.blade.php
+++ source/404.blade.php
@@ -2,7 +2,37 @@
 
 @section('body')
  
-404! Sorry :(
+<div> 
+    <div>
+        <!-- navbar   -->
+        @include('partials.nav')
+
+        <!-- herosection -->
+        <div class="relative max-w-7xl mx-auto mt-20 pb-20">
+            <div class="flex flex-col justify-center items-center px-10 md:px-20">
+                <h1 class="text-4xl font-bold">Error 404 :(</h1>
+
+                <p class="text-lg font-gray-600">
+                    Looks like 
+                    <span class="font-semibold text-lavender-rose">You</span> 
+                    haven't 
+                    <span class="font-semibold text-lavender-rose">Form</span>ed 
+                    a proper URL.
+                </p>
+
+                <p class="text-normal font-gray-500">
+                    Searching for a better form builder?
+                </p>
+
+                <div class="text-center">
+                    <a href="https://app.youform.com/register" class="inline-block mx-auto text-2xl bg-coral border-2 border-black drop-shadow-3xl hover:drop-shadow-4xl  px-10 py-2 text-black rounded-md">
+                        Create free account
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>   
 
 @include('partials.footer')
 
