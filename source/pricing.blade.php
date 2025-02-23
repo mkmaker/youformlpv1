6b3e86e350c5771c15890c8a722949860b783ca7
Abhishek <themkmaker@gmail.com>
2025-02-23 23:16:38 +0530

    Add teams

--- source/pricing.blade.php
+++ source/pricing.blade.php
@@ -585,6 +585,26 @@ description: Youform is free for unlimited forms and submissions. Youform PRO pr
                     </tr>
                     <tr>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
+                            <p class="flex items-center space-x-1" x-tooltip.raw="Organize your forms into workspaces or folders">
+                                <span>Workspaces (or folders) </span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0">                                
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                                </svg>                                  
+                            </p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                            </svg>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
                             <p class="flex items-center space-x-1" x-tooltip.raw="For e.g you can send users to yourwebsite.com after they complete your form.">
                                 <span>Redirect to a URL </span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0">
@@ -732,6 +752,26 @@ description: Youform is free for unlimited forms and submissions. Youform PRO pr
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
                             Unlimited (<a href="https://youform.com/blog/fair-usage-policy" target="_blank" class="text-blue-500 hover:underline">FUP</a>)
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
+                            <p class="flex items-center space-x-1" x-tooltip.raw="Invite team members to your forms and collaborate with them">
+                                <span>Invite team members </span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                                </svg>                                  
+                            </p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                            </svg>
                         </td>
                     </tr>
                     <tr>
