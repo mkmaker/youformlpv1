2b54b00687f41d5d323fd50144da8a5b4d422eee
Abhishek Chakravarty <abhi072592@gmail.com>
2026-02-27 14:45:39 +0000

    Increase embed height to 600px, update copy

--- source/_partials/free-tools-form-embed.blade.php
+++ source/_partials/free-tools-form-embed.blade.php
@@ -19,7 +19,7 @@
                         <svg class="w-5 h-5 text-green-500 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
                         </svg>
-                        <span><strong>40% higher</strong> completion rates than Typeform</span>
+                        <span><strong>40% higher</strong> completion rates than regular forms</span>
                     </li>
                     <li class="flex items-center text-gray-700">
                         <svg class="w-5 h-5 text-green-500 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
@@ -57,7 +57,7 @@
                         src="https://app.youform.com/forms/xrjcjyti" 
                         loading="lazy" 
                         width="100%" 
-                        height="500" 
+                        height="600" 
                         frameborder="0"
                         class="w-full"
                     ></iframe>
